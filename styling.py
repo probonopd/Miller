057807290f363e885b7be1cfa49b95165aa36a12
probonopd <probonopd@users.noreply.github.com>
2025-02-22 08:50:25 +0100

    Show resource paths

--- styling.py
+++ styling.py
@@ -64,7 +64,7 @@ class Styling:
                     "Please download the 'Inter' font family from the following link:\n"
                     "https://fonts.google.com/specimen/Inter\n\n"
                     "After downloading, place the font files in the 'fonts' directory located in the same folder as your script.\n\n"
-                    "Resource path: " + self.resource_path("")
+                    self.resource_path("fonts")
                 )
                 msg_box.setWindowTitle("Font Error")
                 msg_box.exec()
@@ -106,7 +106,8 @@ class Styling:
                 "Oops! It looks like the icon theme is missing.\n\n"
                 "Please download the 'elementary-xfce' icon theme from the following link:\n"
                 "http://archive.ubuntu.com/ubuntu/pool/universe/x/xubuntu-artwork/xubuntu-artwork_16.04.2.tar.xz\n"
-                "and extract 'elementary-xfce' to the 'icons' directory located in the same folder as your script."
+                "and extract 'elementary-xfce' to the 'icons' directory located in the same folder as your script.\n\n"
+                self.resource_path("icons")
             )
             msg_box.setWindowTitle("Icon Theme Error")
             msg_box.exec()
